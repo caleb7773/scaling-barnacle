6aae18f6288dfe852e0bbcff6138c24a93d6f6b1
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2020-07-30 12:17:45 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,57 @@
 #!/bin/bash
-echo test
+sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y
+sudo apt install htop -y
+sudo apt install tree -y
+sudo apt install mlocate -y
+sudo apt install cryptsetup -y
+sudo apt install nmap -y
+sudo apt install fail2ban -y
+
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 /g'
+sudo update-grub
+
+echo 'export PS1="\[$(tput setaf 1)\]\u@\h:\w\$\[$(tput sgr0\] "' >> ~/.bashrc
+source ~/.bashrc
+
+sudo sed -i 's/Port 22/Port 20022/g' /etc/ssh/sshd_config
+sudo systemctl restart ssh
+
+sudo apt install clamav clamav-daemon -y
+sudo apt install chkrootkit rkhunter
+sudo apt install apparmor apparmor-utils apparmor-profiles
+sudo apt install git
+sudo su -
+cd
+git clone https://github.com/CISOfy/lynis.gt
+cd lynis/
+./lynis audit system
+
+sudo apt install aide
+sudo aideinit
+sudo mv /var/lib/aide/aide.db.nw /var/lib/aide/aide.db
+
+sudo chkrootkit
+sudo rkunter --check
+
+sudo vipw | grep 'bin/bash/'
+sudo vigr | grep 'sudo'
+
+sudo cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+#
+#
+#Figure out how to edit the jail.local file with the following
+#
+#port = 20022
+#enabled = true
+#filter = sshd
+#logpath = /var/log/auth.log
+#maxretry = 3
+#bantime = 120
+#
+#
+sudo systemctl restart fail2ban
+sudo systemctl restart ssh
+
+
+sudo reboot
