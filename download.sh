9d03d9660213529aee826c70b9d695cb7214c882
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2020-07-31 13:45:42 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 user_var=$(read -p "Who is the current user? ")
 git clone https://github.com/caleb7773/scaling-barnacle.git
 sudo chmod +x ./scaling-barnacle/startup2.sh
-/home/${USER}/scaling-barnacle/startup2.sh
+/home/${user_var}/scaling-barnacle/startup2.sh
 
 
 
