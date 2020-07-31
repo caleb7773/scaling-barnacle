6947c838030c8fe9d03c828b9aa5397681b5ff19
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2020-07-31 19:44:45 -0400

    Delete download.sh

--- download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "Who is the current user? " user_var
-git clone https://github.com/caleb7773/scaling-barnacle.git
-sudo chmod +x /home/${user_var}/scaling-barnacle/startup2.sh
-/home/${user_var}/scaling-barnacle/startup2.sh
-
-
-
