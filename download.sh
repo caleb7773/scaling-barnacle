ca22e0de24711b8f97cf3fcb32be0edeacddde69
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2020-07-31 13:24:50 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo apt install wget
-wget https://github.com/caleb7773/scaling-barnacle.git
+git clone https://github.com/caleb7773/scaling-barnacle.git
+sudo chmod +x ./scaling-barnacle/startup.sh
+./startup2.sh
+
+
 
