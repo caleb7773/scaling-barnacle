e662ad55e4768d82a61225310fc3b96ed982e900
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2020-07-31 13:34:03 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 git clone https://github.com/caleb7773/scaling-barnacle.git
 sudo chmod +x ./scaling-barnacle/startup2.sh
-cd ~/scaling-barnacle
-./startup2.sh
+/home/${USER}/scaling-barnacle/startup2.sh
 
 
 
