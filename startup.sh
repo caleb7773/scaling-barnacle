ea8ef6a024607667c54088a3a6599fa97b97f91a
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2020-07-31 21:08:17 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,27 @@
 trap 'echo "Cleaning up!"; shred -u /tmp/users; exit' INT
 sudo apt install whois -y
 clear
+
+if [ $(id -u) -eq 0 ]; then
+      read -p "Enter username : " username
+      read -s -p "Enter password : " password
+      egrep "^$username" /etc/passwd >/dev/null
+      if [ $? -eq 0 ]; then
+            echo "$username exists!"
+            exit 1
+      else
+            pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+            useradd -m -s /bin/bash -G sudo -p "$pass" "$username"
+            [ $? -eq 0 ] && echo "User has been added to the system!" || echo "Failed to add a user!"
+      fi
+else
+      echo "Only root may add a user to the system."
+      exit 2
+fi
+read -p "Did it work?" test
+
+
+
 intro_questions() {
       read -p "SSH Port: " ssh_port_num
       echo ' '
