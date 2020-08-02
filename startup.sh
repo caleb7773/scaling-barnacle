d7ed6adc12f12c30ffd97aac003c2723682f63a4
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2020-08-01 20:33:13 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -94,8 +94,12 @@ sudo iptables -A OUTPUT -o lo -j ACCEPT
 sudo iptables -A OUTPUT -p icmp -j ACCEPT
 sudo iptables -P OUTPUT DROP
 sudo iptables -P INPUT DROP
-
-sudo apt-get install iptables-persistent -y
+#############################################################################
+#Installs iptables-persistent without screen prompt for saving IPv4 and IPv6#
+#############################################################################
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+sudo apt-get -y install iptables-persistent
 sudo iptables-save | sudo tee /etc/iptables/rules.v4
 clear
 sudo iptables -nvL
